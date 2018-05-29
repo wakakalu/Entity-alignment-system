655bf18d3f07b9e147dd8c0316391eb16702ea28
wakakalu <diligentapa@gmail.com>
2018-05-29 20:24:16 +0800

    put all tests into one unit test class in order to get test result conveniently

--- test/UnitTest.py
+++ test/UnitTest.py
@@ -6,20 +6,16 @@ import unittest
 import config
 
 
-class TestConfig(unittest.TestCase):
-    def test_read(self):
+class UnitTest(unittest.TestCase):
+    def test_read_config(self):
         dbtype = config.get_config_item(item="dbtype")
         self.assertTrue(isinstance(dbtype, basestring))
 
-
-class TestLogging(unittest.TestCase):
     def test_log(self):
         logger = Logging.get_logger()
         logger.info("This is a info log message")
         logger.error("This is a error log message")
 
-
-class TestHike(unittest.TestCase):
     def test_calc_pp_num_blocks(self):
         sim_mat = array(zeros((10, 10)))
         # [1 3 4 6] for a block
